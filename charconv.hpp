eee4d6ab1eb429b0925a1c76f674c2258912ea03
mojyack <mojyack@gmail.com>
2022-11-17 14:57:48 +0900

    fix charconv namespace

--- charconv.hpp
+++ charconv.hpp
@@ -3,8 +3,12 @@
 #include <optional>
 #include <string_view>
 
+#ifdef CUTIL_NS
+namespace CUTIL_NS {
+#endif
+
 template <class T>
-auto from_chars(const std::string_view& str) -> std::optional<T> {
+auto from_chars(const std::string_view str) -> std::optional<T> {
     auto r = T();
     if(auto [ptr, ec] = std::from_chars(str.begin(), str.end(), r); ec == std::errc{}) {
         return r;
@@ -12,3 +16,7 @@ auto from_chars(const std::string_view& str) -> std::optional<T> {
         return std::nullopt;
     }
 }
+
+#ifdef CUTIL_NS
+}
+#endif
