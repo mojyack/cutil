91c89c76f8cec393947715c60985269abdc08f47
mojyack <mojyack@gmail.com>
2023-03-20 19:09:45 +0900

    modify read_binary

--- misc.hpp
+++ misc.hpp
@@ -8,9 +8,9 @@
 namespace CUTIL_NS {
 #endif
 
-template <class T = std::byte, class CharT>
-auto read_binary(const CharT* const path) -> Result<std::vector<T>> {
-    auto ifs = std::ifstream(path);
+template <class T = std::byte>
+auto read_binary(auto path) -> Result<std::vector<T>> {
+    auto ifs = std::ifstream(std::move(path));
     if(!ifs) {
         return Error("cannot open file");
     }
@@ -25,6 +25,29 @@ auto read_binary(const CharT* const path) -> Result<std::vector<T>> {
         return Error("failed to read file");
     }
     return r;
+}
+
+inline auto split(const std::string_view str, const std::string_view sep) -> std::vector<std::string_view> {
+    auto ret = std::vector<std::string_view>();
+    auto pos = std::string_view::size_type(0);
+    while(true) {
+        if(pos >= str.size()) {
+            break;
+        }
+        const auto prev = pos;
+        pos             = str.find(sep, pos);
+        if(pos == std::string_view::npos) {
+            if(prev != str.size()) {
+                ret.emplace_back(str.substr(prev));
+            }
+            break;
+        }
+
+        ret.emplace_back(str.substr(prev, pos - prev));
+
+        pos += sep.size();
+    }
+    return ret;
 }
 
 #ifdef CUTIL_NS
