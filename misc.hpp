dc294fbcc43fcf1836034e88809b33d1a3cc0ec5
mojyack <mojyack@gmail.com>
2023-05-10 08:58:33 +0900

    add split_args

--- misc.hpp
+++ misc.hpp
@@ -50,6 +50,45 @@ inline auto split(const std::string_view str, const std::string_view sep) -> std
     return ret;
 }
 
+inline auto split_args(const std::string_view str) -> std::vector<std::string_view> {
+    auto       result = std::vector<std::string_view>();
+    const auto len    = str.size();
+    auto       qot    = '\0';
+    auto       arglen = size_t();
+    for(auto i = size_t(0); i < len; i += 1) {
+        while(i < len && std::isspace(str[i])) {
+            i += 1;
+        }
+        if(i >= len) {
+            break;
+        }
+
+        auto start = i;
+        if(str[i] == '\"' || str[i] == '\'') {
+            qot = str[i];
+        }
+        if(qot != '\0') {
+            i += 1;
+            start += 1;
+            while(i < len && str[i] != qot) {
+                i += 1;
+            }
+            if(i < len) {
+                qot = '\0';
+            }
+            arglen = i - start;
+        } else {
+            while(i < len && !std::isspace(str[i])) {
+                i += 1;
+            }
+            arglen = i - start;
+        }
+        result.emplace_back(str.data() + start, arglen);
+    }
+    // internal::dynamic_assert(qot == '\0', "unclosed quotes");
+    return result;
+}
+
 #ifdef CUTIL_NS
 }
 #endif
