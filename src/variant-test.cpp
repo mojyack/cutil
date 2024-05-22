148233807117c433d7b0d7a1454f5b36994db915
mojyack <mojyack@gmail.com>
2024-05-22 14:29:12 +0900

    variant-test: refactor

--- src/variant-test.cpp
+++ src/variant-test.cpp
@@ -22,67 +22,90 @@ auto variant_test() -> void {
         dynamic_assert(!u.is_valid());
     }
     {
-        static auto constructor      = 0;
-        static auto move_constructor = 0;
-        static auto copy_assign      = 0;
-        static auto move_assign      = 0;
-        static auto copy_constructor = 0;
-        static auto destructor       = 0;
+        struct Count {
+            int constructor      = 0;
+            int copy_constructor = 0;
+            int move_constructor = 0;
+            int copy_assign      = 0;
+            int move_assign      = 0;
+            int destructor       = 0;
+        };
+
         struct Noisy {
-            auto operator=(Noisy&&) {
-                move_assign += 1;
+            Count* count;
+            auto   operator=(Noisy&&) {
+                count->move_assign += 1;
             }
 
             auto operator=(Noisy&) {
-                copy_assign += 1;
+                count->copy_assign += 1;
             }
 
-            Noisy() {
-                constructor += 1;
+            Noisy(Count* const count) : count(count) {
+                count->constructor += 1;
             }
 
-            Noisy(Noisy&&) {
-                move_constructor += 1;
+            Noisy(Noisy&& o) {
+                count = o.count;
+                count->move_constructor += 1;
             }
 
-            Noisy(Noisy&) {
-                copy_constructor += 1;
+            Noisy(Noisy& o) {
+                count = o.count;
+                count->copy_constructor += 1;
             }
 
             ~Noisy() {
-                destructor += 1;
+                count->destructor += 1;
             }
         };
 
         using V = Variant<Noisy>;
 
-        auto v = V();
-        dynamic_assert(constructor == 0);
+        // constructor/destructor test
+        auto v  = V();
+        auto cv = Count();
 
-        v.emplace<Noisy>();
-        dynamic_assert(constructor == 1);
+        v.emplace<Noisy>(&cv);
+        dynamic_assert(cv.constructor == 1);
 
         v.reset();
-        dynamic_assert(destructor == 1);
+        dynamic_assert(cv.destructor == 1);
 
-        auto u = V();
-        v.emplace<Noisy>();
+        // copy/move constructor test of contents
+        cv      = Count();
+        auto u  = V();
+        auto cu = Count();
+
+        v.emplace<Noisy>(&cu);
 
         u = v;
-        dynamic_assert(copy_constructor == 1 && v.is_valid());
+        dynamic_assert(cu.copy_constructor == 1 && v.is_valid());
 
         u.reset();
         u = std::move(v);
-        dynamic_assert(move_constructor == 1 && !v.is_valid());
+        dynamic_assert(cu.move_constructor == 1 && !v.is_valid());
 
-        v.emplace<Noisy>();
-        u.emplace<Noisy>();
+        // copy/move assignment test
+        cv = Count();
+        cu = Count();
+        v.emplace<Noisy>(&cv);
+        u.emplace<Noisy>(&cu);
 
         u = v;
-        dynamic_assert(copy_assign == 1 && v.is_valid());
+        dynamic_assert(cu.copy_assign == 1 && v.is_valid());
 
         u = std::move(v);
-        dynamic_assert(move_assign == 1 && !v.is_valid());
+        dynamic_assert(cu.move_assign == 1 && !v.is_valid());
+
+        // constructor test of variant
+        cv = Count();
+        cu = Count();
+
+        auto x = u;
+        dynamic_assert(cu.copy_constructor == 1 && u.is_valid());
+        auto y = std::move(u);
+        dynamic_assert(cu.move_constructor == 1 && !u.is_valid());
     }
 }
 } // namespace
