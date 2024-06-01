fe62f811d495034c95aee12c1e0d40fb49e1a8b0
mojyack <mojyack@gmail.com>
2024-06-01 14:40:46 +0900

    charconv: add base argument

---
+++ src/charconv-test.cpp
@@ -0,0 +1,12 @@
+#include "assert.hpp"
+#include "charconv.hpp"
+
+auto main() -> int {
+    dynamic_assert(from_chars<int>("0") == 0);
+    dynamic_assert(from_chars<int>("-1") == -1);
+    dynamic_assert(from_chars<int>("1") == 1);
+    dynamic_assert(from_chars<int>("8086", 16) == 0x8086);
+    // dynamic_assert(from_chars<double>("0.1") == 0.1); // not supported yet by clang-17
+    print("pass");
+    return 0;
+}
--- src/charconv.hpp
+++ src/charconv.hpp
@@ -7,7 +7,17 @@
 namespace CUTIL_NS {
 #endif
 
-template <class T>
+template <std::integral T>
+auto from_chars(const std::string_view str, const int base = 10) -> std::optional<T> {
+    auto r = T();
+    if(auto [ptr, ec] = std::from_chars(str.begin(), str.end(), r, base); ec == std::errc{}) {
+        return r;
+    } else {
+        return std::nullopt;
+    }
+}
+
+template <std::floating_point T>
 auto from_chars(const std::string_view str) -> std::optional<T> {
     auto r = T();
     if(auto [ptr, ec] = std::from_chars(str.begin(), str.end(), r); ec == std::errc{}) {
