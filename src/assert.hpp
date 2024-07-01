a9c66f454cd615be9eb49b66176b553a56fbc88f
mojyack <mojyack@gmail.com>
2024-07-01 18:19:52 +0900

    assert: print function name

--- src/assert.hpp
+++ src/assert.hpp
@@ -28,12 +28,13 @@ auto dynamic_assert(const bool cond, Args... args) -> void {
 
 struct Location {
     const char* file;
+    const char* function;
     const int   line;
 };
 
 template <class... Args>
 [[noreturn]] auto line_panic(const Location location, Args&&... args) -> void {
-    const auto message = build_string("error at ", location.file, ":", location.line, " ", std::forward<Args>(args)...);
+    const auto message = build_string("error at \"", location.function, "\" at ", location.file, ":", location.line, " ", std::forward<Args>(args)...);
 #ifdef CUTIL_EXCEPTION
     throw std::runtime_error(message);
 #else
@@ -44,12 +45,12 @@ template <class... Args>
 
 template <class... Args>
 auto line_print(const Location location, Args&&... args) -> void {
-    print(location.file, ":", location.line, " ", std::forward<Args>(args)...);
+    print("\"", location.function, "\" at ", location.file, ":", location.line, " ", std::forward<Args>(args)...);
 }
 
 template <class... Args>
 auto line_warn(const Location location, Args&&... args) -> void {
-    warn(location.file, ":", location.line, " ", std::forward<Args>(args)...);
+    warn("\"", location.function, "\" at ", location.file, ":", location.line, " ", std::forward<Args>(args)...);
 }
 
 template <class... Args>
