cfb8e2c83be7d2409eec8f4f933fc6a4b9480893
mojyack <mojyack@gmail.com>
2024-08-05 15:52:50 +0900

    split-test: test split_like_shell

--- src/split-test.cpp
+++ src/split-test.cpp
@@ -7,5 +7,10 @@ auto main() -> int {
         const auto ok   = elms.size() == 3 && elms[0] == "a" && elms[1] == "b" && elms[2] == "c";
         print(ok ? "ok" : "fail");
     }
+    {
+        const auto elms = split_like_shell(R"(a "b c" 'd e')");
+        const auto ok   = elms.size() == 3 && elms[0] == "a" && elms[1] == "b c" && elms[2] == "d e";
+        print(ok ? "ok" : "fail");
+    }
     return 0;
 }
