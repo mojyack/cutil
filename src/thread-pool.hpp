c2610d8024c3a4f0749de3f34a5c44974c75196b
mojyack <mojyack@gmail.com>
2024-08-09 22:41:56 +0900

    add thread-pool

---
+++ src/thread-pool.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <array>
+#include <thread>
+
+#include "multi-event.hpp"
+
+#ifdef CUTIL_NS
+namespace CUTIL_NS {
+#else
+namespace {
+#endif
+
+template <int N>
+struct ThreadPool {
+    std::atomic_int            finished;
+    std::array<std::thread, N> threads;
+    MultiEvent                 event;
+
+    auto run(const auto target) -> void {
+        for(auto& thread : threads) {
+            thread = std::thread([this, target]() {
+                target();
+                finished.fetch_add(1);
+            });
+        }
+    }
+
+    auto stop() -> void {
+        while(finished != N) {
+            event.notify_unblock();
+            std::this_thread::yield();
+        }
+        for(auto& thread : threads) {
+            thread.join();
+        }
+    }
+};
+
+template <class T, int N>
+struct CustomDataThreadPool : ThreadPool<N> {
+    std::array<T, N> data;
+
+    auto run(const auto target) -> void {
+        for(auto i = 0; i < N; i += 1) {
+            this->threads[i] = std::thread([this, target, i]() -> void {
+                target(data[i]);
+                this->finished.fetch_add(1);
+            });
+        }
+    }
+};
+}
