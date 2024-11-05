6dcd79ef74c51565812c7c016fbffa7131e1421b
mojyack <mojyack@gmail.com>
2024-11-05 11:39:49 +0900

    logger: split type definitions to another file

---
+++ src/logger-pre.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <source_location>
+#include <string>
+
+#define CUTIL_MODULE_NAME cutil_logger_v2
+#include "_prologue.hpp"
+
+enum class Loglevel : uint8_t {
+    Error = 0,
+    Warn,
+    Info,
+    Debug,
+
+    Limit,
+};
+
+inline auto string_to_loglevel(std::string_view str) -> std::optional<Loglevel>;
+
+struct Logger {
+    std::string name;
+    Loglevel    loglevel = Loglevel::Info;
+
+    struct FirstArgument {
+        const char*          value;
+        std::source_location location;
+
+        FirstArgument(const char* value, std::source_location location = std::source_location::current())
+            : value(value),
+              location(location) {}
+    };
+
+    template <Loglevel loglevel, class... Args>
+    auto print(FirstArgument format_string, const Args&... args) -> void;
+
+#pragma push_macro("print_alias")
+#define print_alias(name, level)                                  \
+    template <class... Args>                                      \
+    auto name(FirstArgument arg, const Args&... args) -> void {   \
+        print<Loglevel::level, Args...>(std::move(arg), args...); \
+    }
+
+    print_alias(error, Error);
+    print_alias(warn, Warn);
+    print_alias(info, Info);
+    print_alias(debug, Debug);
+#pragma pop_macro("print_alias")
+
+    auto set_name_and_detect_loglevel(std::string name) -> void;
+
+    Logger() = default;
+    Logger(std::string name);
+};
+
+#include "_prologue.hpp"
--- src/logger.hpp
+++ src/logger.hpp
@@ -1,64 +1,15 @@
 #pragma once
 #include <chrono>
-#include <cstdint>
 #include <cstdlib>
 #include <iomanip>
 #include <mutex>
-#include <source_location>
-#include <string>
 
 #include "assert.hpp"
+#include "logger-pre.hpp"
 
 #define CUTIL_MODULE_NAME cutil_logger_v2
 #include "_prologue.hpp"
 
-enum class Loglevel : uint8_t {
-    Error = 0,
-    Warn,
-    Info,
-    Debug,
-
-    Limit,
-};
-
-inline auto string_to_loglevel(std::string_view str) -> std::optional<Loglevel>;
-
-struct Logger {
-    std::string name;
-    Loglevel    loglevel = Loglevel::Info;
-
-    struct FirstArgument {
-        const char*          value;
-        std::source_location location;
-
-        FirstArgument(const char* value, std::source_location location = std::source_location::current())
-            : value(value),
-              location(location) {}
-    };
-
-    template <Loglevel loglevel, class... Args>
-    auto print(FirstArgument format_string, const Args&... args) -> void;
-
-#pragma push_macro("print_alias")
-#define print_alias(name, level)                                  \
-    template <class... Args>                                      \
-    auto name(FirstArgument arg, const Args&... args) -> void {   \
-        print<Loglevel::level, Args...>(std::move(arg), args...); \
-    }
-
-    print_alias(error, Error);
-    print_alias(warn, Warn);
-    print_alias(info, Info);
-    print_alias(debug, Debug);
-#pragma pop_macro("print_alias")
-
-    auto set_name_and_detect_loglevel(std::string name) -> void;
-
-    Logger() = default;
-    Logger(std::string name);
-};
-
-// impl
 inline auto logger_time_base = std::chrono::system_clock::now();
 inline auto stream_lock      = std::mutex();
 
