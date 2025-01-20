ea8ec710b594cf8df2d4fa4bba187ea308a9fc18
mojyack <mojyack@gmail.com>
2025-01-20 18:00:41 +0900

    comptime-string-test: cleanup

--- src/comptime-string-test.cpp
+++ src/comptime-string-test.cpp
@@ -7,27 +7,27 @@ auto main() -> int {
     static_assert(str1[-1] == 'o');
     static_assert(str1.str() == "hello");
 
-    constexpr auto str2 = comptime::concat<str1, comptime::String(" "), comptime::String("world")>;
+    constexpr auto str2 = comptime::concat<str1, " ", "world">;
     static_assert(str2.str() == "hello world");
 
-    static_assert(comptime::starts_with<str2, comptime::String("hello")>);
-    static_assert(!comptime::starts_with<str2, comptime::String("hello!")>);
-    static_assert(comptime::ends_with<str2, comptime::String("world")>);
-    static_assert(!comptime::ends_with<str2, comptime::String("hello!")>);
+    static_assert(comptime::starts_with<str2, "hello">);
+    static_assert(!comptime::starts_with<str2, "hello!">);
+    static_assert(comptime::ends_with<str2, "world">);
+    static_assert(!comptime::ends_with<str2, "hello!">);
 
     static_assert(comptime::substr<str2, 1, 9>.str() == "ello worl");
     static_assert(comptime::substr<str2, 6>.str() == "world");
 
-    static_assert(comptime::find<str2, comptime::String("o")> == 4);
-    static_assert(comptime::find<str2, comptime::String("o"), 5> == 7);
-    static_assert(comptime::rfind<str2, comptime::String("o")> == 7);
-    static_assert(comptime::rfind<str2, comptime::String("o"), 5> == 4);
-    static_assert(comptime::find<str2, comptime::String("!")> == std::string_view::npos);
+    static_assert(comptime::find<str2, "o"> == 4);
+    static_assert(comptime::find<str2, "o", 5> == 7);
+    static_assert(comptime::rfind<str2, "o"> == 7);
+    static_assert(comptime::rfind<str2, "o", 5> == 4);
+    static_assert(comptime::find<str2, "!"> == std::string_view::npos);
 
-    static_assert(comptime::remove_prefix<str2, comptime::String("hello ")>.str() == "world");
-    static_assert(comptime::remove_suffix<str2, comptime::String(" world")>.str() == "hello");
+    static_assert(comptime::remove_prefix<str2, "hello ">.str() == "world");
+    static_assert(comptime::remove_suffix<str2, " world">.str() == "hello");
 
-    static_assert(comptime::replace<str2, comptime::String(" "), comptime::String("___")>.str() == "hello___world");
+    static_assert(comptime::replace<str2, " ", "___">.str() == "hello___world");
 
     static_assert(comptime::to_string<65536>.str() == "65536");
     static_assert(comptime::to_string<0>.str() == "0");
