7355f4dbc200b98fa2c291672dfcf2cf48efda45
mojyack <mojyack@gmail.com>
2025-02-05 17:22:26 +0900

    from_chars: implement fallback from_chars<float> for clang

--- src/charconv-test.cpp
+++ src/charconv-test.cpp
@@ -6,7 +6,7 @@ auto main() -> int {
     dynamic_assert(from_chars<int>("-1") == -1);
     dynamic_assert(from_chars<int>("1") == 1);
     dynamic_assert(from_chars<int>("8086", 16) == 0x8086);
-    // dynamic_assert(from_chars<double>("0.1") == 0.1); // not supported yet by clang-17
+    dynamic_assert(from_chars<double>("0.1") == 0.1);
     std::println("pass");
     return 0;
 }
--- src/charconv.hpp
+++ src/charconv.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <charconv>
+#include <cstdlib>
 #include <optional>
+#include <string>
 #include <string_view>
 
 #define CUTIL_MODULE_NAME cutil_charconv_v1
@@ -18,12 +20,21 @@ auto from_chars(const std::string_view str, const int base = 10) -> std::optiona
 
 template <std::floating_point T>
 auto from_chars(const std::string_view str) -> std::optional<T> {
+    errno = 0;
+    if(const auto r = std::strtod(std::string(str).data(), NULL); errno == 0) {
+        return r;
+    } else {
+        return std::nullopt;
+    }
+
+    /* clang-19 does not suport from_chars<double> yet
     auto r = T();
     if(auto [ptr, ec] = std::from_chars(str.begin(), str.end(), r); ec == std::errc{}) {
         return r;
     } else {
         return std::nullopt;
     }
+    */
 }
 
 #include "_prologue.hpp"
